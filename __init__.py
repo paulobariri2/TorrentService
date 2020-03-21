900f88ce01c7b7b90523671f818fd1ad963a6944
Paulo Barbieri <paulo@MacBook-Air-de-Paulo.local>
2020-03-21 17:44:49 -0300

    Changing download REST input from magnet link to url

--- __init__.py
+++ __init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_restful import Resource, Api, reqparse
 from .torrents import Transmission
 from .torrents import isDownloadRunning
+from .torrents import getMagnetLinkFromPage
 
 app = Flask(__name__)
 
@@ -10,18 +11,21 @@ api = Api(app)
 class Download(Resource):
     def post(self):
         parser = reqparse.RequestParser()
-        parser.add_argument('magnet', required=True)
+        parser.add_argument('url', required=True)
         args = parser.parse_args()
         
-        transm = Transmission(args['magnet'])
-        return transm.startDownload(), 201
+        url = args['url']
+        magnetLink = getMagnetLinkFromPage(url)
+        transm = Transmission(magnetLink)
+        downloadInfo = transm.startDownload()
+        downloadInfo['url'] = url
+        return , 201
 
 class DownloadStatus(Resource):
     def get(self, pid):
         status = {}
         status['pid'] = pid
         status['status'] = "Running" if isDownloadRunning(int(pid)) else "Stopped"
-        
         return status
 
 api.add_resource(Download, '/download')
