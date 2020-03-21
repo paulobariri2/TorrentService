e12773f542442f43a50e9f4b2b7ffbe75f4e82b3
Paulo Barbieri <paulo@MacBook-Air-de-Paulo.local>
2020-03-21 17:35:46 -0300

    Function to get magnet link from page

--- torrents.py
+++ torrents.py
@@ -1,5 +1,9 @@
 import os
 import psutil
+import requests
+from bs4 import BeautifulSoup
+
+TORRENTSITE = "https://1337x.to/"
 
 def isDownloadRunning(pid):
     notRunningStatus = [psutil.STATUS_ZOMBIE, psutil.STATUS_DEAD]
@@ -12,6 +16,18 @@ def isDownloadRunning(pid):
     
     return True
 
+def isMagnetLinkTag(tag):
+    return tag.name == 'a' and "Magnet Download" in tag.text
+
+def getMagnetLinkFromPage(url):
+    resp = requests.get(url)
+    soup = BeautifulSoup(resp.content, 'html.parser')
+    a = soup.find(isMagnetLinkTag)
+    if a and a.get('href'):
+        return a['href']
+    else:
+        raise Exception("Magnet link not found!")
+
 class Transmission:
 
     def __init__(self, magnet):
@@ -20,4 +36,3 @@ class Transmission:
     def startDownload(self):
         pid = os.spawnlp(os.P_NOWAIT, "transmission-cli", "transmission-cli", "-f", "/home/paulo/Documents/python/TorrentService/killtransmission.sh", self.magnet)
         return {"magnet":self.magnet, "pid":pid}
-
