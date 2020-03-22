eb82f66f68bcd4e3ae5ab74b03c667684021e23e
Paulo Barbieri <paulo@MacBook-Air-de-Paulo.local>
2020-03-22 18:57:12 -0300

    Moving files to pacage folder

---
+++ torrent_download/__init__.py
@@ -0,0 +1,45 @@
+from flask import Flask
+from flask_restful import Resource, Api, reqparse
+from .torrents import Transmission
+from .torrents import isDownloadRunning
+from .torrents import getMagnetLinkFromPage
+from .torrents import searchTorrents
+from .torrents import searchTorrentsPages
+
+app = Flask(__name__)
+
+api = Api(app)
+
+class Download(Resource):
+    def post(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('url', required=True)
+        args = parser.parse_args()
+        
+        url = args['url']
+        magnetLink = getMagnetLinkFromPage(url)
+        transm = Transmission(magnetLink)
+        downloadInfo = transm.startDownload()
+        downloadInfo['url'] = url
+        return downloadInfo, 201
+
+class DownloadStatus(Resource):
+    def get(self, pid):
+        status = {}
+        status['pid'] = pid
+        status['status'] = "Running" if isDownloadRunning(int(pid)) else "Stopped"
+        return status
+
+class SearchTorrents(Resource):
+    #def get(self, searchString):
+        #return searchTorrentsPages(searchString), 200
+
+    def get(self, searchString, page=None):
+        if page:
+            return searchTorrents(searchString, int(page)), 200
+        else:
+            return searchTorrentsPages(searchString), 200
+
+api.add_resource(Download, '/download')
+api.add_resource(DownloadStatus, '/download/<string:pid>')
+api.add_resource(SearchTorrents, '/search/<string:searchString>', '/search/<string:searchString>/page/<string:page>')
---
+++ torrent_download/torrents.py
@@ -0,0 +1,95 @@
+import os
+import psutil
+import requests
+from bs4 import BeautifulSoup
+import json
+
+TORRENTSITE = "https://1337x.to%s"
+SEARCHURL = "https://1337x.to/search/%s/%d/"
+
+headers = {
+	"User-Agent": "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.3"
+}
+
+def isDownloadRunning(pid):
+    notRunningStatus = [psutil.STATUS_ZOMBIE, psutil.STATUS_DEAD]
+    try:
+        p = psutil.Process(pid)
+        if p.status() in notRunningStatus:
+            return False
+    except (psutil.NoSuchProcess, psutil.ZombieProcess):
+        return False
+    
+    return True
+
+def isMagnetLinkTag(tag):
+    return tag.name == 'a' and "Magnet Download" in tag.text
+
+def getMagnetLinkFromPage(url):
+    resp = requests.get(url)
+    soup = BeautifulSoup(resp.content, 'html.parser')
+    a = soup.find(isMagnetLinkTag)
+    if a and a.get('href'):
+        return a['href']
+    else:
+        raise Exception("Magnet link not found!")
+
+def getInfoFromHtmlTableRow(row):
+    info = {}
+    for td in row.find_all('td'):
+        if not td.get('class'):
+            continue
+        if 'name' in td['class']:
+            for a in td.find_all('a'):
+                if a.get('class'):
+                    continue
+
+                if a.get('href'):
+                    info['url'] = TORRENTSITE % a['href']
+                    info['name'] = a.text
+
+        if 'seeds' in td['class']:
+            info['seeds'] = td.text
+        if 'coll-date' in td['class']:
+            info['date'] = td.text
+        if 'size' in td['class']:
+            info['size'] = td.text
+        if 'uploader' in td['class']:
+            info['uploader'] = td.text
+    return info
+
+def getJsonFromHtmlTable(table):
+    body = table.find('tbody')
+    if not body:
+        raise Exception("Table has no tbody tag!")
+
+    data = []
+    for row in body.find_all('tr'):
+        torrentInfo = getInfoFromHtmlTableRow(row)
+        if torrentInfo:
+            data.append(torrentInfo)
+    return data
+
+def searchTorrents(searchString, page):
+    resp = requests.get(SEARCHURL % (searchString, page), headers=headers)
+    soup = BeautifulSoup(resp.content, 'html.parser')
+    table = soup.find('table')
+    if not table:
+        raise Exception("No result found for %s, page %d." % (searchString, page))
+
+    return getJsonFromHtmlTable(table)
+
+def searchTorrentsPages(searchString, pages=[1,2,3]):
+    data = []
+    for i in pages:
+        data += searchTorrents(searchString, i)
+    return data
+
+class Transmission:
+
+    def __init__(self, magnet):
+        self.magnet = magnet
+    
+    def startDownload(self):
+        pid = os.spawnlp(os.P_NOWAIT, "transmission-cli", "transmission-cli", "-f", "/home/paulo/Documents/python/TorrentService/killtransmission.sh", self.magnet)
+        return {"magnet":self.magnet, "pid":pid}
