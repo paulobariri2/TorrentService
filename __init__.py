125995025c0c08b18f64e9c8d858e94482c64f82
Paulo Barbieri <paulo@MacBook-Air-de-Paulo.local>
2020-03-21 21:03:29 -0300

    REST GET to search for torrents

--- __init__.py
+++ __init__.py
@@ -3,6 +3,8 @@ from flask_restful import Resource, Api, reqparse
 from .torrents import Transmission
 from .torrents import isDownloadRunning
 from .torrents import getMagnetLinkFromPage
+from .torrents import searchTorrents
+from .torrents import searchTorrentsPages
 
 app = Flask(__name__)
 
@@ -19,7 +21,7 @@ class Download(Resource):
         transm = Transmission(magnetLink)
         downloadInfo = transm.startDownload()
         downloadInfo['url'] = url
-        return , 201
+        return downloadInfo, 201
 
 class DownloadStatus(Resource):
     def get(self, pid):
@@ -28,5 +30,16 @@ class DownloadStatus(Resource):
         status['status'] = "Running" if isDownloadRunning(int(pid)) else "Stopped"
         return status
 
+class SearchTorrents(Resource):
+    #def get(self, searchString):
+        #return searchTorrentsPages(searchString), 200
+
+    def get(self, searchString, page=None):
+        if page:
+            return searchTorrents(searchString, int(page)), 200
+        else:
+            return searchTorrentsPages(searchString), 200
+
 api.add_resource(Download, '/download')
 api.add_resource(DownloadStatus, '/download/<string:pid>')
+api.add_resource(SearchTorrents, '/search/<string:searchString>', '/search/<string:searchString>/page/<string:page>')
